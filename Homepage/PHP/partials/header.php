71243a2466154d14c3b72e53d7e84336f5c0430c
MaarjaKo <165931786+MaarjaKo@users.noreply.github.com>
2025-06-09 14:31:29 +0300

    Uus versioon
    Lisan nii, et header&footer oleks püsivad, siit saab vaikselt edasi minna ja koodi parandama/korrigeerima hakata

---
+++ Homepage/PHP/partials/header.php
@@ -0,0 +1,19 @@
+<header class="header">
+  <div class="menu-icon" aria-label="Menüü">
+    <i class="fas fa-bars" aria-hidden="true"></i>
+  </div>
+  <div class="logo">
+    <a href="../HTML/index.php" aria-label="Avaleht">LOGO</a>
+  </div>
+  <nav class="main-nav">
+    <a href="../HTML/index.php">Avaleht</a>
+    <a href="../HTML/tellimus.php">Tellimus</a>
+    <a href="#">Minu tellimused</a>
+  </nav>
+  <div class="icons">
+    <i class="fas fa-search" aria-hidden="true" aria-label="Otsi"></i>
+    <i class="fas fa-shopping-cart" aria-hidden="true" aria-label="Ostukorv"></i>
+    <i class="fas fa-user" aria-hidden="true" aria-label="Konto"></i>
+    <span id="cart-summary">€ 0.00 | 0 toodet</span>
+  </div>
+</header>
